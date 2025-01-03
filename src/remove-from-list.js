9983a775b9d08bc359088342f8a05bb971409030
renat <murzinarenata@gmail.com>
2025-01-03 11:25:23 -0800

    binary

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,8 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList( l, k) {
+function removeKFromList(l, k) {
 
+  while (l.value === k) {
+    l = l.next;
+  }
+
+  let current = l;
+
+  while (current.next) {
+    if (current.next.value === k) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+
+  return l;
 }
 
 module.exports = {
